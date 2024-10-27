2ff7748c0e9ccce47b2d2eb5cea2e17b6de901b1
huytang05 <tq.quanghuy@gmail.com>
2024-10-27 17:47:51 +0700

    add cmakefile.txt

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include "../include/raylib.h"
+
+int main() {
+  // test raylib
+  // Create a window
+  InitWindow(800, 600, "Hello, World!");
+  SetTargetFPS(60);
+
+  // Main game loop
+  while (!WindowShouldClose()) {
+    // begin drawing
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
+    EndDrawing();
+  }
+
+  return 0;
+}

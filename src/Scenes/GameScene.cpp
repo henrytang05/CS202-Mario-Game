47a5c8e1b8a7627bf453fbbed30ee9d48a371e38
Uyhuynh23 <htuy23@apcs.fitus.edu.vn>
2024-12-14 22:33:58 +0700

    Merge branch 'main' into Uy

--- src/Scenes/GameScene.cpp
+++ src/Scenes/GameScene.cpp
@@ -14,7 +14,7 @@
 class TextureComponent;
 namespace SceneSpace {
 
-GameScene::GameScene() : Scene(), camera({0, 0}) { init(); }
+GameScene::GameScene() : Scene(), camera({0, 0}) {}
 
 void GameScene::init() {
   entityFactory = std::make_unique<EntityFactory>();

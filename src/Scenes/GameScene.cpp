2e4f05fe1b6702357f1bdf786a1873dc9d7f9e9a
huytang05 <tq.quanghuy@gmail.com>
2024-12-16 16:34:34 +0700

    fix a minor string error

--- src/Scenes/GameScene.cpp
+++ src/Scenes/GameScene.cpp
@@ -10,27 +10,26 @@
 #include "Logger.h"
 #include "Scenes/IntroScene.h"
 #include "globals.h"
-class TextureComponent; 
+class TextureComponent;
 namespace SceneSpace {
 
-GameScene::GameScene() : Scene(), camera({0, 0}) {
- 
-} 
- 
+GameScene::GameScene() : Scene(), camera({0, 0}) {}
+
 void GameScene::init() {
   entityFactory = std::make_unique<EntityFactory>();
   player = entityFactory->createMario();
   entities.push_back(player);
-  gameOver = false; 
+  gameOver = false;
   camera.offset = {screenWidth / 2.0f, screenHeight / 2.0f};
-  camera.rotation = 0.0f; 
+  camera.rotation = 0.0f;
   camera.target.x = player->getComponent<PositionComponent>().getPosition().x;
-  camera.target.y = 784.0f - 186.0f; 
-  camera.zoom = 2.0f;  
+  camera.target.y = 784.0f - 186.0f;
+  camera.zoom = 2.0f;
   SoundCtrl.PlayGroundTheme();
-  for(auto &entity : entities) {
-    if(entity->hasComponent<CollisionComponent>()) {
-      entity->getComponent<CollisionComponent>().setEntities(Shared<std::vector<Shared<AbstractEntity>>>(&entities));
+  for (auto &entity : entities) {
+    if (entity->hasComponent<CollisionComponent>()) {
+      entity->getComponent<CollisionComponent>().setEntities(
+          Shared<std::vector<Shared<AbstractEntity>>>(&entities));
     }
   }
 }
@@ -41,19 +40,19 @@ GameScene::~GameScene() {
 #endif
 }
 void GameScene::loadResources() {
-  //Loading BackGround
+  // Loading BackGround
   Image bImage = LoadImage("Map/BackGroundnew.png");
   background = LoadTextureFromImage(bImage);
   UnloadImage(bImage);
-  //Create Map
-  entities = mapRenderer.createMap("Map/level1.json");
+  // Create Map
+  entities = mapRenderer.createMap("Map/Level1.json");
 }
 void GameScene::draw() {
   BeginMode2D(camera);
   DrawTexture(background, 0, 0, WHITE);
   for (auto &entity : entities) {
-    if(entity!=nullptr)
-    entity->draw();
+    if (entity != nullptr)
+      entity->draw();
   }
 
   EndMode2D();

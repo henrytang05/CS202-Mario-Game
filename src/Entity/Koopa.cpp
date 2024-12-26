4aaeed3d9c6bac2c3e2ed34a5c30194d154404bc
huytang05 <tq.quanghuy@gmail.com>
2024-12-26 18:42:58 +0700

    adding Koopa creation

--- src/Entity/Koopa.cpp
+++ src/Entity/Koopa.cpp
@@ -2,6 +2,7 @@
 #include "Entity/Enemy.h"
 #include "EntityManager.h"
 #include "globals.h"
+#include "raylib.h"
 
 Weak<AbstractEntity> initKoopa(Vector2 position, Vector2 size) {
   EntityManager &EM = EntityManager::getInstance();
@@ -19,20 +20,42 @@ Weak<AbstractEntity> initKoopa(Vector2 position, Vector2 size) {
 
   koopa->addComponent<TextureComponent>();
 
-  std::vector<Texture2D> textures{
-      LoadTexture("assets/Goomba/Goomba-Left-Idle.png"),
-      LoadTexture("assets/Goomba/Goomba-Left-Moving.png")};
+  std::string koopaPath = "assets/Koopa/";
+
+  std::string koopaLeftIdle = koopaPath + "Koopa-Left-Idle.png";
+  std::string koopaLeftMoving = koopaPath + "Koopa-Left-Moving.png";
+  std::string koopaRightIdle = koopaPath + "Koopa-Right-Idle.png";
+  std::string koopaRightMoving = koopaPath + "Koopa-Right-Moving.png";
+  std::string shellKoopaIdle = koopaPath + "Shell-Koopa-Idle.png";
+  std::string shellKoopaMoving = koopaPath + "Shell-Koopa-Moving.png";
+  std::string shellKoopaMoving2 = koopaPath + "Shell-Koopa-Moving2.png";
+
+  std::vector<Texture2D> textures = {
+      LoadTexture(koopaLeftIdle.c_str()),
+      LoadTexture(koopaLeftMoving.c_str()),
+  };
+
   koopa->getComponent<TextureComponent>().addTexture("Left-Moving", textures,
                                                      0.1f, true);
 
-  koopa->getComponent<TextureComponent>().addTexture(
-      "Right-Moving",
-      {LoadTexture("assets/Goomba/Goomba-Right-Idle.png"),
-       LoadTexture("assets/Goomba/Goomba-Right-Moving.png")},
-      0.1f, true);
+  textures = {
+      LoadTexture(koopaRightIdle.c_str()),
+      LoadTexture(koopaRightMoving.c_str()),
+  };
+
+  koopa->getComponent<TextureComponent>().addTexture("Right-Moving", textures,
+                                                     0.1f, true);
 
   koopa->getComponent<TextureComponent>().addTexture(
-      "Die", {LoadTexture("assets/Goomba/Goomba-Die.png")}, .3f, false);
+      "Die", {LoadTexture(shellKoopaIdle.c_str())}, .3f, false);
+
+  textures = {
+      LoadTexture(shellKoopaMoving.c_str()),
+      LoadTexture(shellKoopaMoving2.c_str()),
+  };
+
+  koopa->getComponent<TextureComponent>().addTexture("Shell-Moving", textures,
+                                                     0.1f, true);
 
   koopa->getComponent<TextureComponent>().changeState("Left-Moving");
 
